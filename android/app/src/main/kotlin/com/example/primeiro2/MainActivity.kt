1b257872f48b56e82b0e38b99c1007be58e60f1e
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-01-19 09:21:42 -0300

    Criando o projeto

---
+++ android/app/src/main/kotlin/com/example/primeiro2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.primeiro2
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
